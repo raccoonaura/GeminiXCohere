db72b549570f43e983898d2b4a2698a78b9f97cd
Meowaura <ilovececat@gmail.com>
2025-10-05 21:54:33 +0800

    GxC 10052501
    加入reasoning (prefix: @)
    Gemini和Cohere現在記憶會加入的是整合後的回答 而不是各自的
    修復詢問問題時不輸入任何東西即按enter會導致Gemini產生讀取未知圖片的回答 現在會退回到上一行讓使用者正確輸入問題

--- GxC.py
+++ GxC.py
@@ -9,20 +9,24 @@ with open("cohereLog.txt", "w", encoding="utf-8") as file:
 with open("mergedLog.txt", "w", encoding="utf-8") as file:
     pass
 
-def set_marker():
+def setmarker():
     print("\033[s", end="")
 
-def clear_from_marker():
+def clearscreen():
     print("\033[u", end="")
     print("\033[J", end="")
 
-set_marker()
+setmarker()
 client = Client(api_key=input("Enter Gemini API Key: ").strip())
-clear_from_marker()
-set_marker()
+clearscreen()
+setmarker()
 co = cohere.ClientV2(api_key=input("Enter Cohere API Key: ").strip())
-clear_from_marker()
+clearscreen()
+setmarker()
 question = input("Hello! How can I assist you today? ")
+while question.strip() == "":
+    clearscreen()
+    question = input("Hello! How can I assist you today? ")
 
 gMsg = []
 cMsg = []
@@ -34,39 +38,71 @@ mRes = ""
 
 def ask_gemini(question):
     global gRes
-    response = client.models.generate_content_stream(
-        model="gemini-2.5-flash",
-        contents=gMsg,
-        config=types.GenerateContentConfig(
-            thinking_config=types.ThinkingConfig(thinking_budget=0)  # Disables thinking, for token efficiency, as it's enabled by default
-        ),
-    )
-    print ("\n----------\n")
-    for chunk in response:
-        print(chunk.text, end="")  # Real-time printing since the merged response can take a while
-        fullR.append(chunk.text)
-    gRes = ''.join(fullR)  # Join all chunks into a single string for logging and further processing
-    fullR.clear()
-    print ("\n\n----------\n\nGenerating full response...")
+    if question[0] == "@":
+        response = client.models.generate_content_stream(
+            model="gemini-2.5-flash",
+            contents=gMsg,
+            config=types.GenerateContentConfig(
+                thinking_config=types.ThinkingConfig(thinking_budget=-1)  # Dynamic thinking, token usage depends on the complexity of the question
+            ),
+        )
+        for chunk in response:
+            print(chunk.text, end="")  # Real-time printing since the merged response can take a while
+            fullR.append(chunk.text)
+        gRes = ''.join(fullR)  # Join all chunks into a single string for logging and further processing
+        fullR.clear()
+        print ("\n\n----------\n\nGenerating full response...")
+    else:
+        response = client.models.generate_content_stream(
+            model="gemini-2.5-flash",
+            contents=gMsg,
+            config=types.GenerateContentConfig(
+                thinking_config=types.ThinkingConfig(thinking_budget=0)  # Disables thinking, for token efficiency, as it's enabled by default
+            ),
+        )
+        for chunk in response:
+            print(chunk.text, end="")  # Real-time printing since the merged response can take a while
+            fullR.append(chunk.text)
+        gRes = ''.join(fullR)  # Join all chunks into a single string for logging and further processing
+        fullR.clear()
+        print ("\n\n----------\n\nGenerating full response...")
 
 def ask_cohere(question):
     global cRes
-    res = co.chat(
-        model="command-a-03-2025",
-        messages=cMsg,
-    )
-    cRes = res.message.content[0].text
+    if question[0] == "@":
+        response = co.chat(
+            model="command-a-reasoning-08-2025",
+            messages=cMsg,
+        )
+        for content in response.message.content:
+            if content.type == "text":
+                cRes = content.text
+    else:
+        res = co.chat(
+            model="command-a-03-2025",
+            messages=cMsg,
+        )
+        cRes = res.message.content[0].text
 
 def merge_responses(question):
     global gRes, cRes, mMsg, mRes
     mMsg = {"role": "user", "parts": [{"text": "Here are two responses to the same question. Please merge them into a single, try and include most of the non duplicated contents without worrying about the length of the response, and straight export them without explaining my request and what you're going to do.\n\nQuestion: " + question + "\n\nResponse 1: " + gRes + "\n\nResponse 2: " + cRes}]}  # TODO: Find a better way to format this for Gemini, as it's still exporting the prompt in the response
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=mMsg,
-        config=types.GenerateContentConfig(
-            thinking_config=types.ThinkingConfig(thinking_budget=0)  # Disables thinking, for token efficiency, as it's enabled by default
-        ),
-    )
+    if question[0] == "@":
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=mMsg,
+            config=types.GenerateContentConfig(
+                thinking_config=types.ThinkingConfig(thinking_budget=-1)  # Dynamic thinking, token usage depends on the complexity of the question
+            ),
+        )
+    else:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=mMsg,
+            config=types.GenerateContentConfig(
+                thinking_config=types.ThinkingConfig(thinking_budget=0)  # Disables thinking, for token efficiency, as it's enabled by default
+            ),
+        )
     mRes = response.text
 
 while True:  # There could be a better way to do this, but this works for now
@@ -74,11 +110,13 @@ while True:  # There could be a better way to do this, but this works for now
         gMsg.append({"role": "user", "parts": [{"text": question}]})  # Memorize question (Gemini)
         cMsg.append({"role": "user", "content": question})  # Memorize question (Cohere)
 
-        set_marker()
+        setmarker()
         t1 = threading.Thread(target=ask_gemini, args=(question,))
         t2 = threading.Thread(target=ask_cohere, args=(question,))
         t1.start()
         t2.start()
+        if (question[0] == "@"):
+            print ("\n----------\n\nEnabled reasoning! Please wait...\n\n----------\n")
         t1.join()
         t2.join()
         t3 = threading.Thread(target=merge_responses, args=(question,))
@@ -86,7 +124,7 @@ while True:  # There could be a better way to do this, but this works for now
         t3.join()
 
         # print("\n----------\n\nGemini:\n\n", gRes, "\n\n----------\n\nCohere:\n\n", cRes, "\n\n----------\n\nMerged:\n\n", mRes, "\n\n----------\n")
-        clear_from_marker()
+        clearscreen()
         print ("\n----------\n\n", mRes, "\n\n----------\n")
         with open("geminiLog.txt", "a", encoding="utf-8") as file:
             file.write("User:" + question + "\n\n" + "Bot:" + gRes + "\n\n")
@@ -94,8 +132,12 @@ while True:  # There could be a better way to do this, but this works for now
             file.write("User:" + question + "\n\n" + "Bot:" + cRes + "\n\n")
         with open("mergedLog.txt", "a", encoding="utf-8") as file:
             file.write("User:" + question + "\n\n" + "Bot:" + mRes + "\n\n")
-        gMsg.append({"role": "model", "parts": [{"text": gRes}]})  # Memorize response (Gemini)
-        cMsg.append({"role": "assistant", "content": cRes})  # Memorize response (Cohere)
+        gMsg.append({"role": "model", "parts": [{"text": mRes}]})  # Memorize response (Gemini)
+        cMsg.append({"role": "assistant", "content": mRes})  # Memorize response (Cohere)
+        setmarker()
         question = input("Your turn: ")
+        while question.strip() == "":
+            clearscreen()
+            question = input("Your turn: ")
     except Exception as e:
         print("API Key無效或發生錯誤: ", e)
--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # GeminiXCohere
 Gemini + Cohere 多模型聊天整合器  
 
-**Period**: 10/01/25 ~ 01/31/26  
-**Minimum Viable Products**: CLI demo, GitHub repo, Demo的展示影片  
-**Commit標題形式**: 月 + 日 + 年 + 當天第幾個版本  
+Period: 10/01/25 ~ 01/31/26  
+Minimum Viable Products: CLI demo, GitHub repo, Demo的展示影片  
+Commit標題形式: 月 + 日 + 年 + 當天第幾個版本  
+
+已知問題:  
+輸入API時不輸入任何東西就按enter會產生錯誤  
+Gemini的串流回覆太長時 整合過後的回答無法覆蓋全部  
+Gemini整合時有時依然會解釋問題/說明自己要做什麼  
 
 **新增模型步驟**:  
 
